b8112dfd8539656287a99c8f5b855595698eaee3
mvp <dima.bobryshev.mvp@gmail.com>
2020-03-19 03:02:40 +0200

    Different Type & Type Relations

--- schema/schema.js
+++ schema/schema.js
@@ -1,20 +1,41 @@
 const graphql = require(`graphql`);
 
-const {GraphQLObjectType,GraphQLString,GraphQLSchema,GraphQLID} = graphql;
+const {GraphQLObjectType,GraphQLString,GraphQLSchema,GraphQLID,GraphQLInt} = graphql;
 
 
 const movies = [
-    {id:`1`,name:`Pulp Fiction`,genre:`Crime`},
-    {id:`2`,name:`1984`,genre:`Sci-Fi`},
-    {id:3,name:`V for vendetta`,genre:`Action`},
-    {id:4,name:`Snatch`,genre:`Crime-comedy`},
+    {id:`1`,name:`Pulp Fiction`,genre:`Crime`,directorId:`1`},
+    {id:`2`,name:`1984`,genre:`Sci-Fi`,directorId:`2`},
+    {id:3,name:`V for vendetta`,genre:`Action`,directorId:`3`},
+    {id:4,name:`Snatch`,genre:`Crime-comedy`,directorId:`4`},
+]
+const directors = [
+    {id:`1`,name:`Dima`,age:19},
+    {id:`2`,name:`Denis`,age:19},
+    {id:`3`,name:`Stas-O`,age:19},
+    {id:`4`,name:`Stas-R`,age:19},
 ]
 const MovieType = new GraphQLObjectType({
     name:`Movie`,
     fields:()=> ({
-      id:{type:GraphQLString},
+      id:{type:GraphQLID},
       name:{type:GraphQLString},
       genre:{type:GraphQLString},
+        director:{
+          type:DirectorType,
+            resolve(parent,args)
+            {
+                return directors.find(director=>director.id === parent.id);
+            }
+        }
+    }),
+})
+const DirectorType = new GraphQLObjectType({
+    name:`Director`,
+    fields:()=> ({
+      id:{type:GraphQLID},
+      name:{type:GraphQLString},
+      age:{type:GraphQLInt},
     }),
 })
 
@@ -26,9 +47,17 @@ const Query = new GraphQLObjectType({
             args:{id:{type:GraphQLID}},
             resolve(parent,args)
             {
-                return movies.find(movie=>movie.id == args.id);
+                return movies.find(movie=>movie.id === args.id);
             }
-        }
+        },
+        director:{
+            type:DirectorType,
+            args:{id:{type:GraphQLID}},
+            resolve(parent,args)
+            {
+                return directors.find(director=>director.id === args.id);
+            }
+        },
     }
 });
 
